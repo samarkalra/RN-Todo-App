f70d74255324baa8ee8e0f7b9258594c31553b07
samarkalra <skalra@ex2india.com>
2021-05-10 10:56:11 +0530

    Add: Redux Persist to store todoSlice state in AsyncStorage

--- App.js
+++ App.js
@@ -7,20 +7,23 @@ import {
 } from 'react-native';
 import Header from './components/Header';
 import Todos from './components/Todos';
-import store from './Redux/store';
+import {store, persistor} from './Redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <TouchableWithoutFeedback onPress={() => {
-        Keyboard.dismiss();
-      }}>
-        <View style={styles.container}>
-          <Header title='My Todos' />
-          <Todos />
-        </View>
-      </TouchableWithoutFeedback>
+      <PersistGate loading={null} persistor={persistor}>
+        <TouchableWithoutFeedback onPress={() => {
+          Keyboard.dismiss();
+        }}>
+          <View style={styles.container}>
+            <Header title='My Todos' />
+            <Todos />
+          </View>
+        </TouchableWithoutFeedback>
+      </PersistGate>
     </Provider>
   );
 };
--- Redux/store.js
+++ Redux/store.js
@@ -1,8 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { 
+    configureStore, 
+    combineReducers,
+    getDefaultMiddleware,
+} from '@reduxjs/toolkit';
 import todoReducer from './todoSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+} from 'redux-persist/es/constants';
 
-export default configureStore({
-    reducer: {
-        todo: todoReducer
-    }
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whiteList: ['todos'],
+}
+
+const rootReducer = combineReducers({
+    todo: persistReducer(persistConfig, todoReducer),
 });
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: [
+        ...getDefaultMiddleware({
+          thunk: false,
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
+      ],
+});
+
+export const persistor = persistStore(store);
