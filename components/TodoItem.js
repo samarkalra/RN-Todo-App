eb1388ebfdf7b56c782be63ea055fcf12c4b7003
samarkalra <skalra@ex2india.com>
2021-05-17 11:38:08 +0530

    added check box to mark todo completed or not completed

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -6,8 +6,9 @@ import {
     TouchableOpacity
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import CheckBox from '@react-native-community/checkbox';
 import { useDispatch } from 'react-redux';
-import { deleteTodo } from '../Redux/todoSlice';
+import { deleteTodo, toggleTodo } from '../Redux/todoSlice';
 
 const TodoItem = ({ item }) => {
     const dispatch = useDispatch();
@@ -15,32 +16,53 @@ const TodoItem = ({ item }) => {
     const handleTodoDelete = (key) => {
         dispatch(deleteTodo({ key }));
     }
+
+    const handleToggleTodo = (key) => {
+        dispatch(toggleTodo({ key }));
+    }
+
     return (
         <View style={styles.todoContainer}>
-            <Text>{item.text}</Text>
-            <TouchableOpacity onPress={() => handleTodoDelete(item.key)}>
-                <Icon
-                    name='delete'
-                    size={25}
-                    color='coral'
-                />
-            </TouchableOpacity>
+            <CheckBox
+                value={item.isCompleted}
+                onValueChange={() => handleToggleTodo(item.key)}
+            />
+            <View style={styles.rightView}>
+                <Text style={styles.todoText}>{item.text}</Text>
+                <TouchableOpacity onPress={() => handleTodoDelete(item.key)}>
+                    <Icon
+                        name='delete'
+                        size={25}
+                        color='coral'
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     todoContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
         padding: 16,
         marginBottom: 16,
         borderColor: '#bbb',
         borderWidth: 1,
         borderStyle: 'dashed',
         borderRadius: 10,
+    },
+    rightView: {
         flexDirection: 'row',
+        width: '90%',
         justifyContent: 'space-between',
-        alignItems: 'center'
-    }
+        alignItems: 'center',
+    },
+    todoText: {
+        marginLeft: 10
+    },
 });
 
 export default TodoItem;
