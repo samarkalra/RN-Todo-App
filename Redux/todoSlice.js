344ea36bfd8326aaa9a9af1f188eea24e68b6270
samarkalra <skalra@ex2india.com>
2021-05-17 11:37:07 +0530

    added toggleTodo reducer

--- Redux/todoSlice.js
+++ Redux/todoSlice.js
@@ -10,16 +10,23 @@ export const todoSlice = createSlice({
             const { text } = action.payload;
             const newTodo = {
                 text,
-                key: state.todos.length
+                key: state.todos.length,
+                isCompleted: false,
             }
             state.todos.unshift(newTodo);
         },
         deleteTodo: (state, action) => {
             state.todos = state.todos.filter(todo => todo.key !== action.payload.key)
+        },
+        toggleTodo: (state, action) => {
+            const { key } = action.payload;
+            state.todos = state.todos.map(todo => 
+                (todo.key === key) ? {...todo, isCompleted: !todo.isCompleted}: todo
+            )
         }
     }
 });
 
-export const { addTodo, deleteTodo } = todoSlice.actions;
+export const { addTodo, deleteTodo, toggleTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
